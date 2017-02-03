64d0e17f5f34bfea8c7f90691d97eff24c15d695
apjanco <ajanco@haverford.edu>
2017-02-03 12:49:44 -0500

    only add registered students to list

--- moodle_to_csv.py
+++ moodle_to_csv.py
@@ -11,9 +11,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-moodle_user = "USER" #this is your Moodle username
-moodle_pass  = "PASS" #this is your Moodle password
-moodle_course_id = "ID" #this is the id that appears in the URL of your class ...php?id=691 for example
+moodle_user = "ajanco" #this is your Moodle username
+moodle_pass  = "Pushkaisacat881427" #this is your Moodle password
+moodle_course_id = "1165" #this is the id that appears in the URL of your class ...php?id=691 for example
 
 driver = webdriver.Chrome(executable_path="/Users/ajanco/projects/chromedriver") #this is the path to cromedriver, get from https://sites.google.com/a/chromium.org/chromedriver/downloads
 
@@ -34,16 +34,19 @@ driver.get("https://moodle.haverford.edu/enrol/users.php?id=%s" % moodle_course_
 #Now we'll pass the page's HTML to BeautifulSoup, parse it and return values for student names and emails.
 html_content = driver.page_source
 
+
 html_doc     = BeautifulSoup(html_content, 'html.parser')
+
 student_names = html_doc.findAll("div", { "class" : "subfield_userfullnamedisplay" })
 student_emails = html_doc.findAll("div", { "class" : "subfield_email" })
-combined = zip(student_names,student_emails)  
+student_roles = html_doc.findAll("div", { "class" : "role unchangeable" })
+combined = zip(student_names,student_emails,student_roles)  
 
 #this section creates a csv file for the students' names and emails
 file_name = '/Users/ajanco/projects/students_%s.txt' % moodle_course_id
 with open(file_name, 'wb') as fd:
 
-    field_names = ['name','email']
+    field_names = ['name','email','role']
 
     writer = csv.DictWriter(fd,field_names)
 
@@ -54,6 +57,7 @@ with open(file_name, 'wb') as fd:
         row = {}
         row['name']  = name[0].getText()
         row['email'] = name[1].getText()
+        row['role']  = name[2].getText()
 
         writer.writerow(row)
         
